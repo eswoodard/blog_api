da3214ad881540f5a3ced1a6c7b12cd508bbdfa7
Elizabeth Woodard <eswoodard75@gmail.com>
2018-07-05 17:31:03 -0400

    blog post API challenge completed

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+
 const {BlogPosts} = require('./models');
 
 BlogPosts.create("Sample Blog 1", "Lorem ipsum dolor sit amet", "Elizabeth Woodard");
@@ -9,3 +13,52 @@ BlogPosts.create("sample blog 2", "consectetur adipiscing elit", "Sally Student"
 router.get('/', (req, res) => {
     res.json(BlogPosts.get());
 }); 
+
+router.post('/', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+    res.status(201).json(item);
+});
+
+router.delete('/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted blog post \`${req.params.ID}\``);
+    res.status.end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+    const requiredFields = ['id'];
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    if (req.params.id !== req.body.id) {
+        const message = (
+            `Request path id (${req.params.id}) and request body id `
+            `(${req.body.id}) must match`);
+          console.error(message);
+          return res.status(400).send(message);
+        }
+        console.log(`Updating blog posts \`${req.params.id}\``);
+        const updatedItem = BlogPosts.update({
+            id: req.params.id,
+            title: req.body.title,
+            content: req.body.content,
+            publishDate: req.body.publishDate
+        });
+        res.status(204).end();
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const app = express();
+
 const blogPostRouter = require('./blogPostRouter');
+
 
 app.use(morgan('common'));
 
 
 app.use('/blog-post', blogPostRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+  });
